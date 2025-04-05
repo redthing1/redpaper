e658a60449a3eb777431ca2231d82c9f0291faee
redthing1 <redthing1@alt.icu>
2025-04-05 02:49:21 -0700

    update install package

--- resources/install_package.sh
+++ resources/install_package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# set -x
 
 # usage: ./install_package.sh <package_name> <ctan_url>
 
@@ -14,6 +15,7 @@ unzip $PACKAGE_NAME.zip
 cd $PACKAGE_NAME
 
 # generate sty files from ins/dtx if needed
+# first try using ins files, which is the standard method
 for ins in *.ins; do
     [ -f "$ins" ] && latex "$ins"
 done
@@ -21,12 +23,28 @@ done
 # if no sty files generated, try dtx files
 if ! ls *.sty >/dev/null 2>&1; then
     for dtx in *.dtx; do
-        [ -f "$dtx" ] && latex "$dtx"
+        if [ -f "$dtx" ]; then
+            # first try standard latex processing (existing behavior)
+            latex "$dtx"
+            
+            # if that didn't work (circular dependency), try docstrip extraction
+            if ! ls *.sty >/dev/null 2>&1; then
+                # create a temporary docstrip file to extract the package
+                echo '\input docstrip' > temp.ins
+                echo '\generate{\file{'"$PACKAGE_NAME"'.sty}{\from{'"$PACKAGE_NAME"'.dtx}{package}}}' >> temp.ins
+                echo '\endbatchfile' >> temp.ins
+                
+                # run the temporary file with latex
+                latex temp.ins
+            fi
+        fi
     done
 fi
 
 # copy and cleanup
-cp *.sty ~/texmf/tex/latex/$PACKAGE_NAME/
+cp *.sty ~/texmf/tex/latex/$PACKAGE_NAME/ 2>/dev/null || echo "warning: no sty files generated"
+
+# clean up
 cd ..
 rm -rf $PACKAGE_NAME*
 
